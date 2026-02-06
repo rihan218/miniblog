decec45e702eeadc76b0c1a8d510d74efc574776
Rihan Khan <rihanshaikh218@gmail.com>
2026-02-06 22:40:24 +0530

    PROT added

--- app.js
+++ app.js
@@ -49,7 +49,8 @@ mongoose
   .connect("mongodb://127.0.0.1:27017/mini-blog")
   .then(() => {
     console.log("MongoDb connection SUCCESSFUL");
-    app.listen(3000, () => console.log("Server is running on port 3000"));
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log("Server is running on port 3000"));
   })
   .catch((err) => {
     console.log(`MongoDb connection FAILLED: ${err.message}`);
